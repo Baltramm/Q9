0c94095e695b22b21e7c53e703bb688009c55e3d
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-31 19:56:13 +0600

    Добавьте файлы проекта.

---
+++ Q9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Q9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception("Собственное исключения");
+            exception.Data.Add("Дата создания исключения : ", DateTime.Now);
+            exception.HelpLink = "www.google.ru";
+        }
+    }
+}
