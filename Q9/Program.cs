eb511251bafbb8ac56c21d408acc50871eb4f260
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-01 22:05:16 +0600

    Задание 9.3.15

--- Q9/Program.cs
+++ Q9/Program.cs
@@ -4,12 +4,15 @@ namespace Q9
 {
     class Program
     {
-        delegate void ShowMessageDelegate(string _message);
+        delegate int RandomNumberDelegate();
         static void Main(string[] args)
         {
-            ShowMessageDelegate showMessageDelegate = (string _message) => Console.WriteLine(_message);
-          
-            showMessageDelegate.Invoke("Hello World!");
+            RandomNumberDelegate randomNumberDelegate = () =>
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
             Console.Read();
         }
     }
